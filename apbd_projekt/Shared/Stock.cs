dabaa47481e07e3ee951899c0d33da68d3452e90
Vaqxai <vaqxai@gmail.com>
2022-06-23 21:06:58 +0200

    add nicer display for tickers in dashboard search

---
+++ apbd_projekt/Shared/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apbd_projekt.Shared
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public string Name { get; set; }
+        public string PrimaryExchange { get; set; }
+    }
+}
